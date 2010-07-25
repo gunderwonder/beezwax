8f897de1e78e92ce324fd85588921d614e6bfe98
gunderwonder <oysteinrg@gmail.com>
2010-07-25 15:54:59 +0200

    Added dependencies, housekeeping.

---
+++ src/utilities.js
@@ -0,0 +1,24 @@
+Beezwax.symbol = (function() {
+	var counter = 0;
+	return function() { return '__s' + ++counter };
+})();
+
+/**
+ * $E(elementName[, attributes, contents]) -> Element
+ * $E(elementName[, contents]) -> Element
+ * - elementName (String):
+ * - attributes (Object | Hash):
+ * - contents (String | Element | Array):
+ **/
+function $E(elementName, attributes, contents) {	
+	if (Object.isArray(attributes) || Object.isString(attributes) || Object.isElement(attributes)) {
+		contents = attributes;
+		attributes = {};
+	}
+	var element = new Element(elementName, attributes);
+	if (Object.isArray(contents))
+		contents.each(function(c) { element.insert(c); });
+	else
+		element.insert(contents);
+	return element;
+}
