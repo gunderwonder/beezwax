0bba6d8ea0b69a9b31eb0684dafda3829d2b8039
gunderwonder <oysteinrg@gmail.com>
2010-08-06 16:31:31 +0200

    Added low-level functions for loading scripts, JSONP

---
+++ src/ajax.js
@@ -0,0 +1,42 @@
+/**
+ * Beezwax.load(scriptURL[, callback])
+ *  - scriptURL (String): script file to load
+ *  - callback (Function): function to call when script has loaded
+ * Loads a `<script>` with a specified URL asynchronously
+ **/
+Beezwax.load = (function() {
+	var loadedScripts = [];
+	return function load(scriptURL, callback) {
+		callback = callback || Function.EMPTY;
+		if (loadedScripts.include(scriptURL)) {
+			if (callback) callback();
+			return Beezwax;
+		}
+		var script = $E('script', { src : scriptURL, async : 'async' })	;
+		if (callback) {
+			script.observe(script.readyState ? 'readystatechange' : 'load',
+				script.readyState ? function() {
+					if (['loaded', 'complete'].include(script.readyState)) {
+    	            	script.stopObserving('readystatechange');
+    	            	callback();
+					}
+				} : callback
+			);
+    	}
+		$(document.head).insert(script);
+		loadedScripts.push(scriptURL);
+	};
+})();
+
+/**
+ * Beezwax.JSONP(url, callback)
+ **/
+Beezwax.JSONP = function(url, callback, options) {
+	var responder = Beezwax.symbol(),
+		options = Object.extend({ responder : 'callback' }, options || {},
+		url = url + (!url.include('?') ? '?' : '&') + options.responder + '=' + responder;
+	window[responder] = function(data) { 
+		callback(data);
+		delete window[responder];
+	};
+}
