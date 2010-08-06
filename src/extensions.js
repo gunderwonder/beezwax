5969b744d1d587fdfed400bf756c93f7f9bbea57
gunderwonder <oysteinrg@gmail.com>
2010-08-06 19:57:42 +0200

    Removed closure for `window.console` initialization.

--- src/extensions.js
+++ src/extensions.js
@@ -4,10 +4,8 @@ Function.IDENTITY = Function.IDENTITY || function(x) { return x; };
 Function.EMPTY = Function.EMPTY || function() { };
 
 // make sure all console API functions are defined
-(function() {
-	window.console = window.console || {};
-	$w('log debug info warn error assert dir dirxml trace' +
-	' group groupEnd time timeEnd profile profileEnd count').each(function(f) {
-		window.console[f] = window.console[f] || Function.EMPTY;
-	})
-}());
+window.console = window.console || {};
+$w('log debug info warn error assert dir dirxml trace' +
+' group groupEnd time timeEnd profile profileEnd count').each(function(f) {
+	window.console[f] = window.console[f] || Function.EMPTY;
+})
