b3577e9136b044e54cbbadf6a5259c8f267fd8ec
gunderwonder <oysteinrg@gmail.com>
2010-08-13 12:45:11 +0200

    Test `console.log` definition, identity function.

--- test/core.js
+++ test/core.js
@@ -1,5 +1,10 @@
 test('Basic requirements', function() {
 	ok(document.head && document.head.nodeName && document.head.nodeName == 'HEAD', 
 		'document.head should be defined');
-	
+	equal(Function.EMPTY(), undefined, 'Function.EMPTY() == undefined');
+	equal(Function.IDENTITY(1), 1, 'Function.EMPTY(x) == x');
+	$w('log debug info warn error assert dir dirxml trace' +
+		' group groupEnd time timeEnd profile profileEnd count').each(function(f) {
+		ok(typeof console[f] == 'function');
+	})
 })
