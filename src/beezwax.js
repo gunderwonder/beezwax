e39b09e547ca37994e3226cbc2b1e9acdec134c7
Øystein Riiser Gundersen <oysteinrg@gmail.com>
2010-08-22 23:54:41 +0200

    Added `Beezwax.namespace()` for easy namespace definition.

--- src/beezwax.js
+++ src/beezwax.js
@@ -1,7 +1,19 @@
 /* beezwax.js -- Prototype and Scripty 2 extension library
  * Øystein Riiser Gundersen <oysteinrg@gmail.com>
  */
-var Beezwax = { }
+var Beezwax = { 
+	
+	/**
+	 * Beezwax.namespace(namespace) -> undefined
+	 * Define a namespace.
+	 *  http://perfectionkills.com/namespacing-made-easy/
+	 **/
+	namespace : function(namespace) {
+		namespace.split('.').inject(window, function(parent, child) {
+			return parent[child] = parent[child] || {};
+		});
+	}
+}
 
 //= require "extensions"
 //= require "string"
